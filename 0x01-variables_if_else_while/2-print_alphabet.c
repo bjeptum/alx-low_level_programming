3228b0aedc53ab3feea4513a153bb732993aa52b
b_jeptum <brendajeptum@gmail.com>
2022-09-12 16:25:12 -0700

    Alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,12 @@
  *
  * Return: 0 (Success)
  */
-int  main (void)
+int  main(void)
 {
-       char ch;
+	char ch;
 
 	for (ch = ‘a’; ch <= ‘z’; ch++)
 		putchar(ch);
 		putchar(‘\n’);
-	return(0);
+	return (0);
 }
