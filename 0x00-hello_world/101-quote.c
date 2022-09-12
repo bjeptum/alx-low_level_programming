c14305dd49f4e760bd4f7234ef324e8cd59fcea0
b_jeptum <brendajeptum@gmail.com>
2022-09-12 12:39:20 -0700

    UNIX is simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Entrypoint
+ *
+ * Return: 1 (exit)
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
