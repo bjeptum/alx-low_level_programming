7ecc316c7369a053c50509c276719f5265c695d6
b_jeptum <brendajeptum@gmail.com>
2022-09-14 13:18:06 -0700

    Alphabet game _function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet- prints the alphabet in lowercase
+* ch: the character to input
+*
+* Return: 0 (Sucess)
+*/
+int print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define main.h
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
