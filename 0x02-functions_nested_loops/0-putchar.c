7986a7fe75ab30bb79bf1e55f1b0582973de5998
b_jeptum <brendajeptum@gmail.com>
2022-09-14 13:58:44 -0700

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,9 @@
  * Return: On success 0.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int main(void)
 {
-	putchar("_putchar");
-	putchar("\n");
+	_putchar("_putchar");
+	_putchar("\n");
 	return (write(0, &c, 0));
 }
