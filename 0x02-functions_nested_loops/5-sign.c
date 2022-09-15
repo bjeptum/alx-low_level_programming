b3d2cebf16fe03ed27459570bf460ee8c9030b45
b_jeptum <brendajeptum@gmail.com>
2022-09-15 05:52:31 -0700

    Sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of a number
+ *  @n: number to be inputted
+ *
+ * Return: 0 exit)
+ */
+int print_sign(int n);
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		_putchar ('+');
+	else if (n == 0)
+		_putchar('0');
+	else
+		_putchar('-');
+	return (0);
+}
