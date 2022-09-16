972fbf47aa2b17a6b924b635bd1ca35c68d96742
b_jeptum <brendajeptum@gmail.com>
2022-09-16 09:47:22 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * @n: character to be inputted
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		_putchar(n);
+	_putchar('\n');
+	return (0);
+}
