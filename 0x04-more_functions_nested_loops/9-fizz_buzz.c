60c4a13805b937119df35fd37ba744490a3ca420
b_jeptum <brendajeptum@gmail.com>
2022-09-16 15:53:22 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,8 +10,8 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 15 == 0);
-				printf("FizzBuzz\t");
+		if ((i % 15) == 0)
+			printf("FizzBuzz\t");
 		else if ((i % 3) == 0)
 			printf("Fizz\t");
 		else if ((i % 5) == 0)
