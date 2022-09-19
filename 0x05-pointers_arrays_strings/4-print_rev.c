a493c10290966f9d1fbff87bd008b2f5526030ec
b_jeptum <brendajeptum@gmail.com>
2022-09-19 13:40:05 -0700

    Reverse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * print_rev - prints a string in reverse
- * @s - string to be inputted
- * 
+ * @s: string to be inputted
  */
 void print_rev(char *s)
 {
 	int y = 0;
-	
+
 	while (s[y] != '\0')
 	{
 		y++;
