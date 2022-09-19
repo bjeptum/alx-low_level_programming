e797029f055732d8e59cc71efb99e3daf73918f5
b_jeptum <brendajeptum@gmail.com>
2022-09-19 11:23:48 -0700

    98 Battery

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,13 +3,10 @@
  * reset_to_98 - takes a pointer to an int as parameter and updates its value
  * @n: the parameter to be inputted
  *
- * Return: Always 0
  */
 void reset_to_98(int *n)
 {
-	int *ptr_int;
+	int *p = &n;
 
-	ptr_int = &n;
-	*ptr_int = 98;
-	return (0);
+	*p = 98;
 }
