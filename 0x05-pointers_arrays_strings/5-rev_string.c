c9ae9300b478bd72f06ace784fd62ca07280a873
b_jeptum <brendajeptum@gmail.com>
2022-09-19 13:51:56 -0700

    REversed string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses a string
+ * @s: string to be inputted
+ */
+void rev_string(char *s)
+{
+	int y = 0;
+
+	for (y -= 1; y >= 0; y--)
+		while (s[y] != '\0')
+		{
+			y++
+		}
+}
