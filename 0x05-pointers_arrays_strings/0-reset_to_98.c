9ffca662f2be06d373358cb6ac93fa60e29bfb81
b_jeptum <brendajeptum@gmail.com>
2022-09-19 11:04:42 -0700

    98 Battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates its value
+ * @n: the parameter to be inputted
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	int *ptr_int;
+	
+	ptr_int = &n;
+	*ptr_int = 98;
+	return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN.H
 #define MAIN.H
+
 int _putchar(char c);
+void reset_to_98(int *n);
 
 
 #endif
