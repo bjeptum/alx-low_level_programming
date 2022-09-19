efcc07637f25e96502e3173d9edda767cc4246f1
b_jeptum <brendajeptum@gmail.com>
2022-09-19 12:24:00 -0700

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string inputted
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int y = 0;
+
+	while (*s != '\0')
+		s++;
+	{
+		y++;
+	}
+	return (y);
+}
