be5c1cb022364f513129333b8669b8b7f80349a2
b_jeptum <brendajeptum@gmail.com>
2022-09-20 19:58:20 -0700

    Print hals of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ *@str: string to be inputted
+ */
+void puts_half(char *str)
+{
+	int y;
+	int v;
+
+	y = 0;
+
+	for (v = 0; str[v] != 0; v++)
+	{
+		y++;
+	}
+	if (y % 2 == 0)
+	{
+		for (v = y / 2; v < y; v++)
+		{
+			_putchar(s[v]);
+		}
+	}
+	else
+	{
+		for (v = (y / 2) + 1; v < y; v++)
+		{
+			_putchar(s[v];
+		}
+	}
+	_putchar('\n');
+}
