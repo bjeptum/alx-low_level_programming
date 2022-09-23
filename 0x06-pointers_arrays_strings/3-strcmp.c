3dc45a86a3cee24374c1283b81110cdf073b05f6
b_jeptum <brendajeptum@gmail.com>
2022-09-23 00:23:53 -0700

    Comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,19 +7,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 == *s2)
 	{
-		if (*s1 == *s2)
-		{
-			return (0);
-		}
-		else if (*s1 == '\0')
-		{
-			return (-1);
-		}
-		else
+		if (*s1 == '\0' || *s2 == '\0')
+			break;
+		s1++;
+		s2++;
+	}
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (0);
+	}
+	else
 		{
 			return (1);
 		}
-	}
 }
