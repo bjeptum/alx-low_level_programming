2dafbb29b1753666c7f368a9e51825d60189ae97
b_jeptum <brendajeptum@gmail.com>
2022-09-26 13:14:10 -0700

    Memcpy prototype

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	if ((dest != NULL) && (src != NULL))
-	{
-		for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-	}
 	return (0);
 }
