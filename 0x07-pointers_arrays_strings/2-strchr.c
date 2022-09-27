f847d48101cc67e99780ea0019ffd7e5ecc4aaf6
b_jeptum <brendajeptum@gmail.com>
2022-09-26 20:05:30 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,15 +3,16 @@
  * _strchr - finds the first occurrence of a string
  * @s: the string to be checked
  * @c: the character to be searched in s
- * Return NULL
+ * Return: NULL
  */
 char *_strchr(char *s, char c)
 {
-	do {
-		if (*s == c)
-				return (s);
+	if (*s == c)
+	{
+		return (s);
 	}
-	while (*s++);
-
-	return (NULL);
+	else if	(*s++)
+	{
+		return (NULL);
+	}
 }
