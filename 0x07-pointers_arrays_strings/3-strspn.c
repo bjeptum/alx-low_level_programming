4692357561bb9519f26ae10a692f33f4752b7f32
b_jeptum <brendajeptum@gmail.com>
2022-09-27 06:15:50 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,29 @@
 #include "main.h"
 /**
- * _strspn - gets the len gth of a prefix substring
+ * _strspn - gets the length of a prefix substring
  * @s: first string
  * @accept: another string whose characters are searched
  * Return: length of string
  */
 unsigned int _strspn(char *s, char *accept)
+	unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int len;
-	int i;
-	int j;
+		unsigned int len;
+			int i;
+				int j;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] != 32)
-		{
-			for (j = 0; accept[j] != '\0'; j++)
-			{
-				if (s[i] == accept[j])
-					len++;
-			}
-		}
-		else
-			return (len);
-	}
-	return (len);
+					for (i = 0; s[i] != '\0'; i++)
+							{
+										if (s[i] != 32)
+													{
+																	for (j = 0; accept[j] != '\0'; j++)
+																					{
+																										if (s[i] == accept[j])
+																																len++;
+																													}
+																			}
+												else
+																return (len);
+													}
+						return (len);
 }
