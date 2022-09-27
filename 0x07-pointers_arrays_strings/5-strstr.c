1f3d60deaf528ebfbe7b142804716d67ab8693d7
b_jeptum <brendajeptum@gmail.com>
2022-09-27 07:11:06 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,7 @@ char *_strstr(char *haystack, char *needle)
 		if (*haystack != *needle)
 			haystack++;
 		else
-			return (haystack);
+			return (needle);
 	}
 	if (*needle == '\0')
 		return (haystack);
