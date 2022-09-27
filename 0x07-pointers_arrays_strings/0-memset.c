392061abec5b36d783d5a733c56a74539c67a586
b_jeptum <brendajeptum@gmail.com>
2022-09-26 19:58:33 -0700

    strchr

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,7 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 	s = &s[0];
 
-	for (i = 0; i < n ; i++)
+	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (0);
 }
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - finds the first occurrence of a string
+ * @s: the string to be checked
+ * @c: the character to be searched in s
+ * Return NULL
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+				return (s);
+	}
+	while (*s++);
+
+	return (NULL);
+}
