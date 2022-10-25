08af96125a7348df94731c823a6bf83d657277c3
b_jeptum <brendajeptum@gmail.com>
2022-10-25 11:53:24 -0700

    Update files

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-
-
-int _putchar(char c);
-size_t print_list(const list_t *h);
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -17,11 +12,15 @@ size_t print_list(const list_t *h);
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif/* LISTS_H */
