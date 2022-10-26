c645400e9a36c7df3a04a0498cea455b5415c7c4
b_jeptum <brendajeptum@gmail.com>
2022-10-26 02:40:37 -0700

    First structure created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - new structure
+* @name: first element
+* @age: second element
+* @owner: third element
+*
+* Description: new structure of type dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
