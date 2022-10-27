0bbb2658d83847b21b77a1aacb10dece14c87071
b_jeptum <brendajeptum@gmail.com>
2022-10-27 12:59:15 -0700

    Count of elements in list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* listint_len - returns the number of elements
+* @h: head of list
+* Return: number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
