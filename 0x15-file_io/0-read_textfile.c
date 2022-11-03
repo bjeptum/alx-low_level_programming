efc989da1e592310a48f3fc0afb7a3269967f8ce
b_jeptum <brendajeptum@gmail.com>
2022-11-03 07:49:53 -0700

    Read a text file and print it

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,35 +3,45 @@
 
 #include<sys/types.h>
 #include<sys/stat.h>
-#include <fcntl.h>  
+#include<fcntl.h>
 /**
 * read_textfile - reads a text file and prints it to stdout
 * @filename: name of file
 * @letters: letters to be read
-* Return: number of bytes 
+* Return: number of bytes
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buff = NULL;
 	ssize_t fd, r, w;
 
-	if(!filename)
+	if (!filename || !letters)
 		return (0);
 
-	buff = malloc(sizeof(char)*(letters + 1));
+	buff = malloc(sizeof(char) * (letters + 1));
 
-	if(!buff)
+	if (!buff)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	r = read(fd, buff, letters);
-	w = write(STDOUT_FILENO, buff, r);
-	
-	if(!w)
+	if (fd == -1)
+	{
+		free(buff);
 		return (0);
+	}
+	r = read(fd, buff, letters);
+	if (r == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buff, r);
 
 	free(buff);
 	close(fd);
 
-	return (letters);
+	if (w != r)
+		return (0);
+	return (r);
 }
