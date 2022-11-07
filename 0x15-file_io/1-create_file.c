a14677c48d67184117b59a7f475ebe7bdcbfae0b
b_jeptum <brendajeptum@gmail.com>
2022-11-07 01:38:38 -0800

    Create a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w,len;
+	int o, w, len;
 
 	if (filename == NULL)
 		return (-1);
@@ -17,13 +17,13 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	o = open(filename, O_CREAT|O_RDWR|O_TRUNC|0000);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0000);
 	if (o == -1)
 	{
 		close(o);
 	}
-	w = write(o,text_content,len);
-	if(w == -1)
+	w = write(o, text_content, len);
+	if (w == -1)
 	{
 		close(0);
 	}
