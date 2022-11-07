32aa2031a7f8e9ddf5bbaa11a83e98f2f5afca3c
b_jeptum <brendajeptum@gmail.com>
2022-11-07 05:00:50 -0800

    Create a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,23 +8,28 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len;
+	int o;
+	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0000);
+	o = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 06006);
 	if (o == -1)
 	{
-		close(o);
+		return (-1);
 	}
-	w = write(o, text_content, len);
-	if (w == -1)
+	while (text_content[len])
 	{
+		len++;
+		if (text_content == NULL)
+		{
+			close(o);
+			return (-1);
+		}
+		else
+		{
+			write(o, text_content, len);
+		}
 		close(0);
 	}
 	return (1);
