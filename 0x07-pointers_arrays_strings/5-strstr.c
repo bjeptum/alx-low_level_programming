9f947d3744a793e8e4d4d8649c760b7c1412656f
b_jeptum <brendajeptum@gmail.com>
2022-12-13 08:41:13 -0800

    Locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,18 +1,41 @@
+#include <stdlib.h>
+/**
+ * _strncmp - compares two strings within b bytes
+ * @s1: string to check
+ * @s2: string to check from
+ * @b: number of bytes to check
+ * Return: 0 if the strings are the same
+ */
+int _strncmp(char *s1, char *s2, unsigned int b)
+{
+	unsigned int i;
+	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
+	{
+		s1++;
+		s2++;
+	}
+	return (i - b);
+}
+
 #include "main.h"
 /**
  * _strstr - locates a substring
  * @haystack: main C string to be scanned
  * @needle: the smaller string to be searched within the haystack
- * Return: returns a  poineter to first occurrence
+ * Return: returns a  pointer to first occurrence
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
+	unsigned int len;
+
+	len = 0;
+	while (needle[len] != '\0')
+		len++;
+	while (*haystack != '\0')
 	{
-		if (*haystack != *needle)
-			haystack++;
-		else
-			return (needle);
+		if (_strncmp(haystack, needle, len) == 0)
+			return (haystack);
+		haystack++;
 	}
 	return (NULL);
 }
