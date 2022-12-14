da70a93cd9061d3995d23adcab370764ef0b4767
b_jeptum <brendajeptum@gmail.com>
2022-12-14 00:15:12 -0800

    Update strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,19 +7,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' || *s2 != '\0')
-		if (*s1 == *s2)
-		{
-			s1++;
-			s2++;
-		}
-		else if ((*s1 == '\0' && *s2 == '\0') || (*s1 != *s2))
-		{
-			return (1);
-			break;
-		}
-		else 
-		{
-			return (0);
-		}
+	int i = 0;
+	int j = 0;
+	
+	while (s1[i] != '\0' && j == 0)
+	{
+		j = s1[i] - s2[i];
+		i++;
+	}
+	return (j);
 }
