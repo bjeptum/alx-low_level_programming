2b2b1770e752053dbb824d938081c0124cc2e920
bjeptum <brendajeptum@gmail.com>
2023-11-17 15:45:09 +0000

    updated function to resolve segmentation fault

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,16 +6,35 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
+	if (n == 0 || n == 1)
 	{
-		return (0);
-	}
-	else if (n != 0)
-	{
-		return (_sqrt_recursion(n));
+		return (n);
 	}
 	else
 	{
+		return (find_sqrt(n, 1));
+	}
+}
+
+/**
+ * find_sqrt - helper function to find the square root recursively
+ * @n: number to find natural square root
+ * @sqrt: current guess for the square root
+ * Return: Always  -1 if no natural square root for n
+ */
+int find_sqrt(int n, int sqrt)
+{
+	if (sqrt * sqrt == n)
+	{
+		return (sqrt);
+	}
+	else if (sqrt * sqrt > n)
+	{
 		return (-1);
+	}
+	/* Recursive case: increment sqrt and continue the search */
+	else
+	{
+		return (find_sqrt(n, sqrt + 1));
 	}
 }
