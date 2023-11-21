be7f35eff5e20efd64a9737b93753b3d423e5f91
bjeptum <brendajeptum@gmail.com>
2023-11-21 21:15:02 +0000

    Return a pointer to a new string which is a duplicate of the string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strdup - pointer to newly allocated space in memory
+ * @str: copy of the string
+ * Return: Always NULL if str is empty
+ */
+char *_strdup(char *str)
+{
+	int str_size;
+	static char *cp;
+	char *cp_offset;
+
+	/* Allocate memory for duplicate */
+	str_size = strlen(str);
+	cp = (char *) malloc(sizeof(char) * str_size + 1);
+	if (cp == NULL)
+	{
+		return ((char *)NULL);
+	}
+	/* Copy string */
+	cp_offset = cp;
+	while (*str)
+	{
+		*cp_offset = *str;
+		cp_offset++;
+		str++;
+	}
+	*cp_offset = '\0';
+	return (cp);
+}
