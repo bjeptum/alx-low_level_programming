0e7b3d2785b49785d51151f741e2f152fd8fe962
bjeptum <brendajeptum@gmail.com>
2023-11-27 19:22:00 +0000

    A program that prints the name of the file it was cpmpiled from using Macros

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - prints the name of the file
+ * Return: Always name of file
+ */
 #define _FILE_ printf("%s\n", __FILE__)
-
-int main() {
+int main(void)
+{
 	_FILE_;
-    return 0;
+	return (0);
 }
