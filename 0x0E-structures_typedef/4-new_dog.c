d4d889165e2b2ae6f2d06ed1cb9f553560733dc5
bjeptum <brendajeptum@gmail.com>
2023-11-29 15:10:03 +0000

    Create  a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,14 +13,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = malloc(sizeof(dog_t));
 
-	if (d != NULL)
+	if (d == NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-
-		return (d);
+		free(d);
+		return (NULL);
 	}
 
-	return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
 }
