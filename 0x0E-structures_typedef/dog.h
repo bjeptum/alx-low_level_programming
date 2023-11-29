ae6b7c2f13207276cb04778f04de058810f6a593
bjeptum <brendajeptum@gmail.com>
2023-11-29 09:32:48 +0000

    Define a new type of struct dog_t of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,12 +19,7 @@ struct dog
  * @age: member variable 2
  * @owner: member variable 3
  */
-typedef struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
