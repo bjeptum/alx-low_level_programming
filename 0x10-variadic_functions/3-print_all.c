2c46b355475fe3d104210d207c63c5603aabbd08
bjeptum <brendajeptum@gmail.com>
2023-12-01 10:37:39 +0000

    Prints anything that is a list of any format type of arguments updated#2

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,14 +11,11 @@ void print_all(const char * const format, ...)
 	int val;
 	char *str;
 	unsigned int i = 0;
-	int separator = 0;
 
 	va_start(argList, format);
 
 	while (format && format[i])
 	{
-		if (separator && format[i - 1] != '\0')
-			printf(", ");
 		switch (format[i])
 		{
 			case 'c':
@@ -36,9 +33,14 @@ void print_all(const char * const format, ...)
 				str = va_arg(argList, char*);
 				(str != NULL) ? printf("%s", str) : printf("(nil)");
 				break;
+			default:
+				break;
 		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+					|| format[i] == 's') && format[i + 1] != '\0')
+			printf(", ");
+
 		i++;
-		separator = 1;
 	}
 	printf("\n");
 	va_end(argList);
