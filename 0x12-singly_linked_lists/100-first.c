12e28ed14a943853ffcb20bc886694567815d85a
bjeptum <brendajeptum@gmail.com>
2023-12-11 15:52:23 +0000

    Run first before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_first - prints to stdout before the main function
+ * Return: Always 0 on failure
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must"
+			"allow,\nI bore my house upon my back!\n");
+}
+
+/**
+ * main - main function
+ * Return:Always 0 on failure
+ *
+ */
+int main(void)
+{
+	print_first();
+
+	return (0);
+}
