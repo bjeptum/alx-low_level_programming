1cef37743a7a75ad4e2a83c23b6372b62e23f165
bjeptum <brendajeptum@gmail.com>
2023-12-12 21:21:56 +0000

    Free a linked list with head set to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to head of list
+ * Return: ALways 0 on failure
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
