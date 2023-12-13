d1c5563fa29411b0ed8fda24a3e6d34fa4019b40
bjeptum <brendajeptum@gmail.com>
2023-12-13 14:42:49 +0000

    Allocate memory for an array using malloc (myown calloc)

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: elements in array
+ * @size: byte size of eachelement
+ * Return: Always NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int total_size;
+	unsigned int i;
+	void *ptr;
+
+	total_size = nmemb * size;
+	if (nmemb != 0 && total_size / nmemb != size)
+	{
+		return (NULL);
+	}
+	ptr = malloc(total_size);
+
+	if (ptr != NULL)
+	{
+		for (i = 0; i < total_size; i++)
+		{
+			*((char *)ptr + i) = 0;
+		}
+	}
+	return (ptr);
+}
