79f9663c96bd3b409aa69ada1898d16f022a44a2
bjeptum <brendajeptum@gmail.com>
2024-01-12 08:13:18 +0000

    Copy contents ofone file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * main - copy contents of one file to another
+ * @argc: count of inputted arguments
+ * @argv: array ofarguments
+ *
+ * Return: 0 else, -1  on failure
+ */
+int main(int argc, char **argv)
+{
+	FILE *file_from;
+	FILE *file_to;
+	int c;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+	file_from = fopen(argv[1], "r");
+	if (file_from == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = fopen(argv[2], "w");
+	if (file_to == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	c = fgetc(file_from);
+	while (c != EOF)
+	{
+		fputc(c, file_to);
+	}
+	if (fclose(file_from) == EOF)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileno(file_from));
+		exit(100);
+	}
+	if (fclose(file_to) == EOF)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileno(file_to));
+		exit(100);
+	}
+	return (0);
+}
